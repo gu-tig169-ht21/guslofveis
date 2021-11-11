f9683c129bfddf043d819a28a7a4ef684b424e10
eiihpos <guslofveis@student.gu.se>
2021-11-11 20:21:09 +0100

    Filterfunktionen + test av drawer

--- lib/main.dart
+++ lib/main.dart
@@ -14,34 +14,79 @@ class MyApp extends StatelessWidget {
 }
 
 class MainView extends StatelessWidget {
-  List <String> todo = ['Plugga', 'Städa', 'Matlagning', 'Handla', 'Träna', 'Umgås'];
+  List<String> todo = [
+    'Plugga',
+    'Städa',
+    'Matlagning',
+    'Handla',
+    'Träna',
+    'Umgås'
+  ];
+  List<String> filter = ['Alla', 'Gjorda', 'Ogjorda'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green[100],
-        appBar: AppBar(
-          title: Text('TIG169 - TODO', style: TextStyle(fontSize: 30)), 
-          backgroundColor: Colors.green,
-          centerTitle: true,
-          actions: [
-            IconButton(
-              icon: Icon(Icons.filter_alt_outlined),
-              onPressed: () {}),
+      appBar: AppBar(
+        title: const Text('TIG169 - TODO', style: TextStyle(fontSize: 30)),
+        backgroundColor: Colors.green,
+        centerTitle: true,
+        actions: [
+          PopupMenuButton(
+              itemBuilder: (context) => [
+                    PopupMenuItem(child: Text('Visa alla')),
+                    PopupMenuItem(child: Text('Gjorda')),
+                    PopupMenuItem(child: Text('Att göra'))
+                  ]),
+        ],
+      ),
+      body: ListView.builder(
+          itemCount: todo.length,
+          itemBuilder: (context, index) {
+            return _listItem(todo[index]);
+          }),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.green,
+              ),
+              child: Text('Filtrera', style: TextStyle(fontSize: 35)),
+            ),
+            ListTile(
+              title: const Text('Alla', style: TextStyle(fontSize: 18)),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Gjorda', style: TextStyle(fontSize: 18)),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Ogjorda', style: TextStyle(fontSize: 18)),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
           ],
         ),
-        body: ListView.builder(itemCount: todo.length, itemBuilder: (context, index){
-          return _listItem(todo[index]);
-        }),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add, size: 50),
-          backgroundColor: Colors.green,
-          onPressed: () {
-            Navigator.push(context,
-              MaterialPageRoute(builder: (context) => AddView()));
-          },
-        ),
-      );
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add, size: 50),
+        backgroundColor: Colors.green,
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => AddView()));
+        },
+      ),
+    );
   }
 
   Widget _listItem(text) {
@@ -52,7 +97,7 @@ class MainView extends StatelessWidget {
         child: ListTile(
           leading: Checkbox(value: false, onChanged: (value) {}),
           title: Text(text, style: TextStyle(fontSize: 25)),
-          trailing: Icon(Icons.delete, size: 30.0), 
+          trailing: Icon(Icons.delete, size: 30.0),
         ),
       ),
     );
@@ -62,24 +107,23 @@ class MainView extends StatelessWidget {
 class AddView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green[100],
-      appBar: AppBar(
-        title: Text("Lägg till TODO"),
-        backgroundColor: Colors.green,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(height: 30),
-            _card(),
-            Container(height: 30),
-            _buttonRow(),
-          ],
+        backgroundColor: Colors.green[100],
+        appBar: AppBar(
+          title: Text("Lägg till TODO"),
+          backgroundColor: Colors.green,
         ),
-      ) 
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(height: 30),
+              _card(),
+              Container(height: 30),
+              _buttonRow(),
+            ],
+          ),
+        ));
   }
 
   Widget _card() {
@@ -87,13 +131,12 @@ class AddView extends StatelessWidget {
       child: Column(
         children: [
           Card(
-            margin: EdgeInsets.only(left: 30, right: 30, top: 10),
-            child: TextField(
-              textAlign: TextAlign.center,
-          decoration: InputDecoration(
-            hintText: 'Vad vill du lägga till på din TODO-lista?' ),
-            )
-          )
+              margin: EdgeInsets.only(left: 30, right: 30, top: 10),
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                    hintText: 'Vad vill du lägga till på din TODO-lista?'),
+              ))
         ],
       ),
     );
@@ -104,11 +147,11 @@ class AddView extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         RaisedButton(
-          onPressed: () {}, 
+          onPressed: () {},
           child: Text('Lägg till'),
           color: Colors.green,
           textColor: Colors.white,
-          ),
+        ),
       ],
     );
   }
